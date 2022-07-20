34a8827796930c54d2782da0464e8c6210e7de82
Surendra <SURENDRA@GMAIL.COM>
2022-07-20 17:23:05 +0530

    first commit

---
+++ demo.rb
@@ -0,0 +1,17 @@
+# Difference Between instance_of?, kind_of? and is_a? methods
+
+
+class Parent
+
+end
+
+
+class Child < Parent
+
+end
+
+obj1 = Child.new
+
+puts obj1.kind_of?(Parent)
+puts obj1.is_a?(Parent)
+puts obj1.instance_of?(Child)
---
+++ variable_scope.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Demo class code
+class Demo
+  def initialize(name, id)
+    @name = name
+    @id = id
+  end
+
+  # method_first code
+  def method_first
+    puts "#{@name} #{@id}"
+  end
+
+  # method_second code
+  def method_second
+    puts "#{@name} #{@id}"
+  end
+end
+
+obj = Demo.new('Surendra', '101')
+obj1 = Demo.new('sandeep', '102')
+obj.method_first
+obj1.method_second
